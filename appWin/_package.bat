f8c14bbf39e227b93fa101cb74d3ff211ea9eaf9
mojoco <>
2018-02-26 16:36:28 +0100

    tsa

--- appWin/_package.bat
+++ appWin/_package.bat
@@ -20,9 +20,13 @@ echo.
 echo.
 echo ------------
 
+::	call %SDK_HOME%\bin\adt.bat -package -storetype "pkcs12" -keystore "certificate.p12" -storepass "1234" -target bundle "Main.exe" "Main-app.xml" "Main.swf"
+
+:: -tsa none
 ::	call %SDK_HOME%\bin\adt.bat -package -storetype "pkcs12" -keystore "certificate.p12" -tsa "none" -storepass "1234" -target bundle "Main.exe" "Main-app.xml" "Main.swf"
-	
-	call %SDK_HOME%\bin\adt.bat -package -storetype "pkcs12" -keystore "certificate.p12" -storepass "1234" -target bundle "Main.exe" "Main-app.xml" "Main.swf"
+
+:: -tsa http://sha256timestamp.ws.symantec.com/sha256/timestamp
+	call %SDK_HOME%\bin\adt.bat -package -storetype "pkcs12" -keystore "certificate.p12" -tsa http://sha256timestamp.ws.symantec.com/sha256/timestamp -storepass "1234" -target bundle "Main.exe" "Main-app.xml" "Main.swf"
 
 echo ------------
 echo.
