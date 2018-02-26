1d8aab3b9eb4f6a88a8b2cd902c20cd609b22aa5
mojoco <>
2018-02-26 16:48:37 +0100

    tsa

--- appWin/_package.bat
+++ appWin/_package.bat
@@ -11,22 +11,24 @@
 
 echo.
 echo ------------
+echo AIR Developer Tool
 
 	call %SDK_HOME%\bin\adt.bat -version
 	
-echo ------------
 echo.
 
-echo.
-echo ------------
-
+:: BAD !!!
 ::	call %SDK_HOME%\bin\adt.bat -package -storetype "pkcs12" -keystore "certificate.p12" -storepass "1234" -target bundle "Main.exe" "Main-app.xml" "Main.swf"
 
-:: -tsa none
-::	call %SDK_HOME%\bin\adt.bat -package -storetype "pkcs12" -keystore "certificate.p12" -tsa "none" -storepass "1234" -target bundle "Main.exe" "Main-app.xml" "Main.swf"
+:: OK
+::		-tsa none
+::		-tsa http://sha256timestamp.ws.symantec.com/sha256/timestamp
 
-:: -tsa http://sha256timestamp.ws.symantec.com/sha256/timestamp
-	call %SDK_HOME%\bin\adt.bat -package -storetype "pkcs12" -keystore "certificate.p12" -tsa http://sha256timestamp.ws.symantec.com/sha256/timestamp -storepass "1234" -target bundle "Main.exe" "Main-app.xml" "Main.swf"
+	call %SDK_HOME%\bin\adt.bat												^
+		-package															^
+		-storetype "pkcs12" -keystore "certificate.p12"	-storepass "1234"	^
+		-tsa http://sha256timestamp.ws.symantec.com/sha256/timestamp		^
+		-target bundle "Main.exe" "Main-app.xml" "Main.swf"
 
 echo ------------
 echo.
