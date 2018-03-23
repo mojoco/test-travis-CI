0a26f6ad07b377208a891d3669f607e2a807d66c
mojoco <>
2018-03-23 12:44:39 +0100

    ..

---
+++ appveyor-to-gh-pages-branch.cmd
@@ -0,0 +1,33 @@
+
+
+echo =================================================
+echo ------ starting deploy to GitHub Pages.. (branch)
+echo =================================================
+echo:
+
+:: where..
+mkdir out
+cd out
+
+:: permission..
+git config --global user.email "mojoco@gmail.com"
+git config --global user.name "mojoco"
+
+:: what..
+git clone --branch "gh-pages" https://github.com/mojoco/lab-ci.git .
+
+:: modify.. (append)
+::echo:>>README.md
+::echo ^#^#^#^# AppVeyor CI (build %appveyor_build_version%)>>README.md
+
+:: modify.. (replace)
+PowerShell -Command "(Get-Content 'deploy\github-pages\index.html') -replace '.*AppVeyor CI.*', 'AppVeyor CI (build %appveyor_build_version%)' | Out-File -encoding utf8 'deploy\github-pages\index.html'"
+
+git add 'deploy\github-pages\index.html'
+git commit --message "Update GitHub Pages.[skip ci]"
+
+:: send..
+git push --force --quiet "https://%GITHUB_TOKEN%@github.com/mojoco/lab-ci"
+
+:: cleanup..
+cd ..
