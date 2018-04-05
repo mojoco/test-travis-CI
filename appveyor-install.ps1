3a3a99cf54b2bf4c80d145235b782f4a6a3bad5c
mojoco <>
2018-04-05 23:23:32 +0200

    ..

--- appveyor-install.ps1
+++
@@ -1,5 +0,0 @@
-
-if ( !(Test-Path "$env:APPVEYOR_BUILD_FOLDER\$env:SDK_TARGET.zip"))
-{
-	Start-FileDownload https://github.com/mojoco/$env:SDK_REPO/archive/$env:SDK_TARGET.zip -FileName $env:APPVEYOR_BUILD_FOLDER\$env:SDK_TARGET.zip
-}
