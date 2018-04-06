dcfc6ffd5b0c086776613036267bb2d6f352e0f7
mojoco <>
2018-04-06 13:56:45 +0200

    ..

--- appveyor-install.cmd
+++ appveyor-install.cmd
@@ -13,18 +13,28 @@
 
 :: install sdk.. (if no exist)
 
+if exist %APPVEYOR_BUILD_FOLDER%\sdk\flex (
+	echo ----- use cached SDK !!! ----- %SDK_TARGET%.zip
+	EXIT 0
+)
+
 if not exist %APPVEYOR_BUILD_FOLDER%\sdk\flex (
 
 	echo ---
 	echo ---
-	mkdir %APPVEYOR_BUILD_FOLDER%\sdk\flex
+	echo ---
+	echo ---
+	echo ---
+	mkdir %APPVEYOR_BUILD_FOLDER%\sdk
 	echo ----- download SDK !!! ----- %SDK_TARGET%.zip
 	:: ( --location -> redo the request on the new place, if the server reports that the requested page has moved to a different location )
-	curl --location --output %APPVEYOR_BUILD_FOLDER%\sdk\flex\%SDK_TARGET%.zip https://github.com/mojoco/%SDK_REPO%/archive/%SDK_TARGET%.zip
+	curl --location --output %APPVEYOR_BUILD_FOLDER%\sdk\%SDK_TARGET%.zip https://github.com/mojoco/%SDK_REPO%/archive/%SDK_TARGET%.zip
 	echo ---
 	echo ---
 	dir %APPVEYOR_BUILD_FOLDER%\sdk
-	dir %APPVEYOR_BUILD_FOLDER%\sdk\flex
+	echo ---
+	echo ---
+	echo ---
 	echo ---
 	echo ---
 
