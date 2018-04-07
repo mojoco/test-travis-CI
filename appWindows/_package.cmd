2f414dea863a18761017e200ba9f7fde7c7d2f96
mojoco <legrec@legrec.com>
2018-04-07 12:44:00 +0200

    Update _package.cmd

--- appWindows/_package.cmd
+++ appWindows/_package.cmd
@@ -1,21 +1,21 @@
 @echo on
 
-:: use current directory
+REM use current directory
 	cd /d %~dp0
 
 echo.
 echo ------------
 echo packaging..
 
-:: BAD !!!
-::	call %SDK_HOME%\bin\adt.bat -package -storetype "pkcs12" -keystore "certificate.p12" -storepass "1234" -target bundle "Main.exe" "Main-app.xml" "Main.swf"
+REM BAD !!!
+REM	call %SDK_HOME%\bin\adt.bat -package -storetype "pkcs12" -keystore "certificate.p12" -storepass "1234" -target bundle "Main.exe" "Main-app.xml" "Main.swf"
 
-:: OK
-::		-tsa none
-::		-tsa http://sha256timestamp.ws.symantec.com/sha256/timestamp
+REM OK
+REM		-tsa none
+REM		-tsa http://sha256timestamp.ws.symantec.com/sha256/timestamp
 
-	adt																		^
-		-package															^
+	adt										^
+		-package							^
 		-storetype "pkcs12" -keystore "certificate.p12"	-storepass "1234"	^
 		-tsa http://sha256timestamp.ws.symantec.com/sha256/timestamp		^
 		-target bundle "Main.exe" "Main-app.xml" "Main.swf"
