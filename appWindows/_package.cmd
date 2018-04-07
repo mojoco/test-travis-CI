46ad4648e736b325ba0ae29f9cb78725f1699b23
mojoco <legrec@legrec.com>
2018-04-07 12:34:03 +0200

    Update _package.cmd

--- appWindows/_package.cmd
+++ appWindows/_package.cmd
@@ -5,6 +5,7 @@
 
 echo.
 echo ------------
+echo packaging..
 
 :: BAD !!!
 ::	call %SDK_HOME%\bin\adt.bat -package -storetype "pkcs12" -keystore "certificate.p12" -storepass "1234" -target bundle "Main.exe" "Main-app.xml" "Main.swf"
@@ -19,5 +20,8 @@ echo ------------
 		-tsa http://sha256timestamp.ws.symantec.com/sha256/timestamp		^
 		-target bundle "Main.exe" "Main-app.xml" "Main.swf"
 
+dir .
+dir Main.exe
+zip Main.exe Main.zip
 echo ------------
 echo.
