99d815675b3bec49c522c5d67b4a12433eb11a44
mojoco <legrec@legrec.com>
2018-04-08 19:43:09 +0200

    Update appveyor-deploy-to-gh-pages-branch.cmd

--- appveyor-deploy-to-gh-pages-branch.cmd
+++ appveyor-deploy-to-gh-pages-branch.cmd
@@ -22,33 +22,13 @@ git clone --branch "gh-pages" https://github.com/mojoco/test-travis-CI.git .
 
 :: modify.. (replace)
 PowerShell -Command "(Get-Content '.\index.html') -replace '.*AppVeyor CI.*', 'AppVeyor CI (build %APPVEYOR_BUILD_VERSION%)' | Out-File -encoding utf8 '.\index.html'"
-
 git add index.html
 
 del ".\Main.zip"
 copy "%APPVEYOR_BUILD_FOLDER%\appWindows\Main.zip" ".\Main.zip"
-echo:
-echo:
-echo:
-dir %APPVEYOR_BUILD_FOLDER%\appWindows
-echo:
-echo:
-dir .
-echo:
-echo:
-echo:
-echo:
 git add ".\Main.zip"
 
 :: send..
 git commit --message "Update GitHub Pages.[skip ci]"
-
-::									! [rejected]        gh-pages -> gh-pages (fetch first)
-
 git pull
-
-::git push --force --quiet "https://%GITHUB_TOKEN%@github.com/mojoco/test-travis-CI"
 git push --force  "https://%GITHUB_TOKEN%@github.com/mojoco/test-travis-CI"
-
-:: cleanup..
-cd ..
